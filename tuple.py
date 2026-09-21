3a95b31c371d4d7e4e8eae2bfff23063bca9f8f5
MonicaGodwin <monicagodwinn@gmail.com>
2026-09-22 00:15:38 +0100

    my commit

---
+++ tuple.py
@@ -0,0 +1,11 @@
+tpl =  tuple()
+tpl = ("Blessing", "Roselyn", "Comfort")
+tpl1 = ("kate", "Pauline", "Favour")
+joined_tpl = tpl + tpl1
+family_member = list(joined_tpl)
+family_member.append("Agnes")
+family_member.append("Godwin")
+
+print(joined_tpl)
+print(len(joined_tpl))
+print(family_member)
